34fcdcb70b98b98eb3e3b55e4c6afa1feeb7f80a
David Seibert <david@seibert.io>
2020-03-11 00:06:31 +0100

    feat: follow crawl redirects

--- src/services/CrawlService.php
+++ src/services/CrawlService.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author David Seibert<david@seibert.io>
  */
@@ -10,6 +11,8 @@ use craft\base\Component;
 use craft\elements\Entry;
 use craft\helpers\UrlHelper;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\TransferStats;
 use seibertio\elasticsearch\ElasticSearchPlugin;
 use seibertio\elasticsearch\exceptions\MarkupExtractionException;
 
@@ -30,14 +33,18 @@ class CrawlService extends Component
     public function init()
     {
         parent::init();
-        $this->initClient();
+        $this->initClient([
+            'read_timeout' => 30,
+        ]);
     }
 
-    public function initClient(array $config = []) {
+    public function initClient(array $config = [])
+    {
         $this->client = new Client($config);
     }
 
-    public function getClient(): Client {
+    public function getClient(): Client
+    {
         return $this->client;
     }
 
@@ -61,46 +68,62 @@ class CrawlService extends Component
         // Generate the sharable url based on the previously generated token
         $url = UrlHelper::urlWithToken($fetchUrl ?? $entry->getUrl(), $token);
 
-        // replace base url with the base url set in plugin config.
-        // this is required for scenarios where the app may not resolve its own hostname and requires to use a different one to access itself
-        // this happens e.g. in local docker environments where the app url is http://localhost as seen from the host machine
-        $url = str_replace(UrlHelper::baseUrl(), ElasticSearchPlugin::$plugin->getSettings()->getFetchBaseUrl(), $url);
+        $url = $this->replaceBaseUrl($url);
 
         return $url;
     }
 
     public function fetchURL($url)
     {
-        $response = $this->getClient()->request('GET', $url);
-        return $response->getBody();
-	}
-	
-	public function extractIndexableMarkup($markup): string {
-		// search for page segments explicitly allowed to be indexed.
-			// if none are found, assume the entire body may be indexed
-			$allowRegex = '/<!--\s?search:allow\s?-->(.*)<!--\s?\/search:allow\s?-->/Us';
-			if (preg_match_all($allowRegex, $markup, $matches, PREG_SET_ORDER, 0) > 0) {
-				$matches = array_map(fn($match) => $match[1], $matches);
-			} else {
-				$bodyRegex = '/<body[^>]*>(.*)<\/body>/si';
-				if (preg_match($bodyRegex, $markup, $matches, PREG_OFFSET_CAPTURE, 0) === 1) {
-					$matches = [$matches[1][0]];
-				}
-				else {
-					$message = 'Could not parse document - no HTML body found and no explicit search:allow comments exist';
-					Craft::error($message, 'elasticsearch');
-					throw new MarkupExtractionException($message);
-				}
-			}
-			
-			$content = '';
+        $redirectedUrl = $url;
+        try {
+            $response = $this->getClient()->request('GET', $url, [
+                'on_stats' => function (TransferStats $stats) use (&$redirectedUrl) {
+                    // follow redirects
+                    $redirectedUrl = $this->replaceBaseUrl((string) $stats->getEffectiveUri());
+                }
+            ]);
+            return $response->getBody();
+        } catch (ConnectException $e) {
+            if ($redirectedUrl !== $url) return $this->fetchURL($redirectedUrl);
+            throw $e;
+        }
+    }
 
-			foreach ($matches as $match) {
-				$denyRegex = '/<!--\s?search:deny\s?-->(.*)<!--\s?\/search:deny\s?-->/Us';
-				$indexableContent = preg_replace($denyRegex, '', $match);
-				$content .= $indexableContent;
-			}
+    public function extractIndexableMarkup($markup): string
+    {
+        // search for page segments explicitly allowed to be indexed.
+        // if none are found, assume the entire body may be indexed
+        $allowRegex = '/<!--\s?search:allow\s?-->(.*)<!--\s?\/search:allow\s?-->/Us';
+        if (preg_match_all($allowRegex, $markup, $matches, PREG_SET_ORDER, 0) > 0) {
+            $matches = array_map(fn ($match) => $match[1], $matches);
+        } else {
+            $bodyRegex = '/<body[^>]*>(.*)<\/body>/si';
+            if (preg_match($bodyRegex, $markup, $matches, PREG_OFFSET_CAPTURE, 0) === 1) {
+                $matches = [$matches[1][0]];
+            } else {
+                $message = 'Could not parse document - no HTML body found and no explicit search:allow comments exist';
+                Craft::error($message, 'elasticsearch');
+                throw new MarkupExtractionException($message);
+            }
+        }
 
-			return $content;
-	}
+        $content = '';
+
+        foreach ($matches as $match) {
+            $denyRegex = '/<!--\s?search:deny\s?-->(.*)<!--\s?\/search:deny\s?-->/Us';
+            $indexableContent = preg_replace($denyRegex, '', $match);
+            $content .= $indexableContent;
+        }
+
+        return $content;
+    }
+
+    private function replaceBaseUrl($url): string
+    {
+        // replace base url with the base url set in plugin config.
+        // this is required for scenarios where the app may not resolve its own hostname and requires to use a different one to access itself
+        // this happens e.g. in local docker environments where the app url is http://localhost as seen from the host machine
+        return str_replace(UrlHelper::baseUrl(), ElasticSearchPlugin::$plugin->getSettings()->getFetchBaseUrl(), $url);
+    }
 }
