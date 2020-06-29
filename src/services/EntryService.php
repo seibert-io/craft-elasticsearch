054dfbbf96aa054ccecf97a83ea92e11b5bde819
David Seibert <david@seibert.io>
2020-06-29 15:29:03 +0200

    fix: honor enabledForSite entry setting

--- src/services/EntryService.php
+++ src/services/EntryService.php
@@ -6,7 +6,6 @@
 
 namespace seibertio\elasticsearch\services;
 
-use benf\neo\elements\Block;
 use Craft;
 use craft\base\Component;
 use craft\elements\Entry;
@@ -26,63 +25,18 @@ use seibertio\elasticsearch\jobs\IndexEntryJob;
  */
 class EntryService extends Component
 {
-    /**
-     * Retrieves a list of the entry, its descendants and all of its related entries to an entry within the given entries site
-     * (based on a relation chain where the given entry is the original target element,
-     * bubbling upwards recursively from there on)
-     *
-     * @param Entry $entry
-     * @return Entry[]
-     */
-    private function getEntryAndRelatedEntries(Entry $entry, $includeDescendants = true): array
-    {
-        $ancestors = [];
-
-        if ($entry->enabled) {
-            $ancestors[] = $entry;
-
-            // find regular field relations
-            $relatedEntries = Entry::find()->site($entry->site)->relatedTo(['targetElement' => $entry])->unique()->all();
-            if ($includeDescendants) {
-                $relatedEntries = array_merge($relatedEntries, $entry->getDescendants(1)->all());
-            }
-            /*
-            // find neo block relations
-            $blocks = Block::find()->relatedTo(['targetElement' => $entry])->unique()->all();
-            $entriesTargetingBlock = Entry::find()->id(array_map(fn ($block) => $block->ownerId, $blocks))->all();
-            $relatedEntries = array_merge($relatedEntries, $entriesTargetingBlock);
-            */
-            foreach ($relatedEntries as $ancestor) {
-                /** @var Entry $relatedEntry */
-                if (ElementHelper::isDraftOrRevision($ancestor)) {
-                    continue;
-                }
-
-                foreach ($this->getEntryAndRelatedEntries($ancestor, false) as $ancestorEntry) {
-
-                    if (sizeof(array_filter($ancestors, fn ($ancestor) => $ancestor->id === $ancestorEntry->id)) === 0) {
-                        $ancestors[] = $ancestorEntry;
-                    }
-                }
-            }
-        }
-
-        return $ancestors;
-    }
-
     public function handleEntryUpdate(Entry $entry): void
     {
         if (!ElasticSearchPlugin::$plugin->getSettings()->getAutoIndexEntries()) return;
 
         if (ElementHelper::isDraftOrRevision($entry)) return;
 
-        //$entries = $this->getEntryAndRelatedEntries($entry);
         $entries = [$entry];
 
         foreach ($entries as $entryToProcess) {
             if (ElementHelper::isDraftOrRevision($entryToProcess)) continue;
 
-            if (!$entryToProcess->enabled) {
+            if (!$entryToProcess->enabled || !$entryToProcess->enabledForSite) {
                 $job = new DeleteEntryJob(['entryId' => $entryToProcess->getId(), 'siteId' => $entryToProcess->siteId]);
             } else {
                 $indexableSectionHandles = ElasticSearchPlugin::$plugin->getSettings()->getIndexableSectionHandles();
